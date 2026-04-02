b0e7d318862ca35e9fadb61091f54929a698bb80
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2026-04-02 14:59:05 -0600

    Add causal mask and temperature

--- src/chatGnT/models/predict.py
+++ src/chatGnT/models/predict.py
@@ -1,29 +1,40 @@
 import torch
 
-def predict(model, device, pad_id, vocab, inv_vocab, ids, max_len=50):
+def predict(model, device, pad_id, vocab, inv_vocab, start_ingred, max_len=50, temperature=0.8):
     model.eval()
+
+    # Start sequence with group of tokens: amt, unit, ingred, sep
+    ids = [vocab[i] for i in start_ingred]
 
     for _ in range(max_len):
         x = torch.tensor(ids, dtype=torch.long, device=device).unsqueeze(1)
 
+        # padding mask
+        pad_mask = (x == pad_id).transpose(0, 1)  # (batch, seq_len)
+
+        # causal mask
         seq_len = x.size(0)
-        # pad_mask = (x == pad_id).transpose(0, 1)
-        # src_mask = model.generate_square_subsequent_mask(seq_len).to(device)
-        #TODO: do I want these for prediction?
+        src_mask = model.generate_square_subsequent_mask(seq_len).to(device)
 
         with torch.no_grad():
             output = model(
                 src=x,
-                src_key_padding_mask=None,
-                src_mask=None
+                src_key_padding_mask=pad_mask,
+                src_mask=src_mask
             )
 
             logits = output[-1, 0, :]
-            # Convert to probabilities
-            probs = torch.softmax(logits, dim=-1)
-            # Get most likely next token
-            #TODO: how to introduce temperature here?
-            next_id = torch.argmax(probs).item()
+
+            # block repeated <sep>
+            last_token = inv_vocab[ids[-1]]
+            if last_token == "<sep>":
+                logits[vocab["<sep>"]] = -float("inf")
+
+            # Sampling for diversity
+            # Convert to probabilities with temperature
+            probs = torch.softmax(logits / temperature, dim=-1)
+            # Sample for next token
+            next_id = torch.multinomial(probs, num_samples=1).item()
 
         # Append prediction
         ids.append(next_id)
@@ -57,9 +68,9 @@ def predict_groups(model, device, pad_id, vocab, inv_vocab, start_ingred, max_gr
     """
     model.eval()
     
-    # Start sequence with the ingredient
-    generated_ids = [vocab[start_ingred]]
-    tokens = [start_ingred]
+    # Start sequence with group of tokens: amt, unit, ingred, sep
+    generated_ids = [vocab[i] for i in start_ingred]
+    tokens = start_ingred
     
     for _ in range(max_groups):
         group_ids = []
@@ -68,14 +79,18 @@ def predict_groups(model, device, pad_id, vocab, inv_vocab, start_ingred, max_gr
             x = torch.tensor(generated_ids + group_ids, dtype=torch.long, device=device).unsqueeze(1)
             seq_len = x.size(0)
             
-            # pad_mask = (x == pad_id).transpose(0, 1)
-            # src_mask = model.generate_square_subsequent_mask(seq_len).to(device)
+            # padding mask
+            pad_mask = (x == pad_id).transpose(0, 1)  # (batch, seq_len)
+
+            # causal mask
+            seq_len = x.size(0)
+            src_mask = model.generate_square_subsequent_mask(seq_len).to(device)
             
             with torch.no_grad():
                 output = model(
                     src=x,
-                    src_key_padding_mask=None,
-                    src_mask=None)
+                    src_key_padding_mask=pad_mask,
+                    src_mask=src_mask)
 
                 logits = output[-1, 0, :]
                 
@@ -99,11 +114,4 @@ def predict_groups(model, device, pad_id, vocab, inv_vocab, start_ingred, max_gr
         if token == "<end>":
             break
     
-    # Optional: clean repeated <sep>
-    clean_tokens = []
-    for t in tokens:
-        if clean_tokens and t == "<sep>" and clean_tokens[-1] == "<sep>":
-            continue
-        clean_tokens.append(t)
-    
-    return clean_tokens
+    return tokens
